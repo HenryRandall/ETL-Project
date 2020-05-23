3126f72158a5ba31b9d773603da45cc789341ce9
unknown <henrywrandall@gmail.com>
2020-05-23 13:03:40 -0400

    Fixed bug in pulling the first death date

---
+++ config.py
@@ -0,0 +1,10 @@
+# Enter your API key
+username='xmzprnlngzpyxd'
+
+password='6a83946f53ee7e08eb55866da51975f550d4414df0c47e99c74565f0c328a087'
+
+host='ec2-35-153-12-59.compute-1.amazonaws.com'
+
+port='5432'
+
+database='dfnrgd738l7tse'
